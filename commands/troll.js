c089b6579a5f9092f2ce6052a225d5bd6432b04c
Aladdin <g.tabroid@gmail.com>
2021-04-17 01:59:37 +0300

    v 1.1.2

--- commands/troll.js
+++ commands/troll.js
@@ -3,9 +3,18 @@ const trollings = require('../data/trollings');
 
 module.exports = {
   name: 'troll',
-  description: 'Троллит ебаного Воронина',
+  description: 'Троллит, притом жейско',
   aliases: ['t'],
-  async execute(ctx) {
-    ctx.send(trollings.default[rand.int(trollings.default.length)]);
+  async execute(ctx, args) {
+    let s = trollings.default; // selected trolligns array
+    const randomTrolling = () => s[rand.int(s.length)];
+
+    if (!args || !args.length) {
+      ctx.send(randomTrolling());
+      return;
+    }
+
+    s = trollings.all;
+    ctx.send(`${args.join(' ')} ${randomTrolling()}`);
   },
 };
--- functions/schedule.js
+++ functions/schedule.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const filterArray = require('../utility/filterArray');
 const { serverAddress } = require('../config');
 
 module.exports = {
@@ -9,39 +8,13 @@ module.exports = {
 
     if (start) url.searchParams.append('start', start);
     if (finish) url.searchParams.append('finish', finish);
+
     return fetch(url.href)
       .then(async (res) => {
         const json = await res.json();
-        const { schedule } = json || {};
 
         // if request error
         if (!res.ok) throw new Error(res.statusText);
-
-        if (typeof schedule === 'object' && Array.isArray(schedule) && schedule.length) {
-          const scheduleLength = schedule ? schedule.length : 0;
-          const indexesToDelete = [];
-
-          // compare two nearest array elements
-          for (let i = 0; i <= scheduleLength - 2; i += 2) {
-            const c = schedule[i]; // current elements
-            const n = schedule[i + 1]; // next element
-            if (!n) break; // stop loop, if next element not exists
-
-            if (
-              (c.date === n.date)
-              && (c.discipline === n.discipline)
-              && (c.kindOfWork === n.kindOfWork)
-              && (c.lecturer === n.lecturer)
-            ) {
-              // combine two array elements
-              schedule[i].endLesson = n.endLesson;
-              indexesToDelete.push(i + 1);
-            }
-          }
-
-          // filter array
-          return { schedule: filterArray(schedule, indexesToDelete) };
-        }
 
         return json;
       })
