2066439cd3e90ad7dba5b9c149b63ad3afc1847e
Aladdin <g.tabroid@gmail.com>
2021-07-24 15:50:57 +0300

    v 1.17.2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { VK } = require('vk-io');
 const events = require('./data/events');
 const commands = require('./data/commands');
 const cron = require('./data/cron');
-// const parser = require('./data/parser');
 const { token } = require('./config');
 
 const vk = new VK({ token });
@@ -15,7 +14,6 @@ vk.updates.start()
       console.info('[CRON] has been started');
     });
 
-    // setTimeout(() => parser.init(vk), 2000);
     console.info('[BOT] has been started');
   })
   .catch(console.error);
