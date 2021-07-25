5a8f5a92c4cfc0eae07c8fffa0494e321f62ecbb
Aladdin <g.tabroid@gmail.com>
2021-07-25 17:51:43 +0300

    v 1.18.1

--- commands/actuality.js
+++ commands/actuality.js
@@ -14,19 +14,21 @@ module.exports = {
   ],
   async execute(ctx, args) {
     pactuality.get()
-      .then((actuality) => {
+      .then(({ actuality }) => {
         const [command] = args;
         const msg = [];
 
         if (command && command.toLowerCase() === 'lazy') {
+          if (!actuality.lazyContent) return ctx.send('Несрочная актуалочка пуста 😔');
           msg.push('Несрочное актуалити\n');
           msg.push(actuality.lazyContent);
         } else {
+          if (!actuality.content) return ctx.send('Актуалочка пуста 😔');
           msg.push(`Актуалити. Обновлено: ${format(actuality.date)}\n`);
           msg.push(`${actuality.content}\n`);
         }
 
-        ctx.send(msg.join('\n'));
+        return ctx.send(msg.join('\n'));
       })
       .catch(() => {
         ctx.send(texts.status.databaseError);
