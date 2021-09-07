c35bdce0a3f0d2b9c538241dd2e7e50ea0342534
Aladdin <g.tabroid@gmail.com>
2021-09-07 23:47:46 +0300

    v 1.21.1

--- commands/currentTime.js
+++ commands/currentTime.js
@@ -1,9 +1,10 @@
 const { formatDate } = require('../helpers');
 
 module.exports = {
-  name       : 'time',
-  description: 'текущее время по МСК',
-  arguments  : [{ name: 'format', description: 'Формат вывода https://date-fns.org/v2.23.0/docs/format' }],
+  name              : 'time',
+  description       : 'текущее время по МСК',
+  arguments         : [{ name: 'format', description: 'Формат вывода https://date-fns.org/v2.23.0/docs/format' }],
+  lowercaseArguments: false,
   execute(ctx, args) {
     const [format] = args;
     const today = formatDate(Date.now(), format || 'HH:mm');
