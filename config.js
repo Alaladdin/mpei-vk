3154af3ebfef8bf18ea21dfb9ed14069a7f37390
Aladdin <g.tabroid@gmail.com>
2021-10-30 12:54:32 +0300

    v 1.24.5

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   isProd,
   prefix          : ['/', '!', '@'] || process.env.PREFIXES,
   token           : isProd ? process.env.TOKEN : process.env.TOKEN_DEV,
-  serverAddress   : isProd ? process.env.PROD_SERVER : process.env.DEV_SERVER,
+  serverAddress   : process.env.SERVER_ADDRESS,
   authToken       : process.env.AUTH_TOKEN,
   serverDateFormat: 'yyyy.MM.dd',
   outImagePath    : path.resolve(__dirname, './files/userCreated.png'),
