a8f48ff50635095c1a8cb89b3c9cc3c754684460
Aladdin <g.tabroid@gmail.com>
2022-02-22 02:59:40 +0300

    v 1.27.3

--- cron/scheduleSubscribe.js
+++ cron/scheduleSubscribe.js
@@ -1,53 +1,58 @@
 const schedule = require('node-schedule');
 const { each } = require('lodash');
 const { serverDateFormat } = require('../config');
-const { formatDate, removeFromDate, handleError } = require('../helpers');
+const { formatDate, getDateDiffInMinutes, handleError } = require('../helpers');
 const { getters: storeGetters } = require('../store');
 const { getSchedule: getRawSchedule, getFormattedSchedule, sendAsImage } = require('../functions');
 
 module.exports = {
-  currentSchedule: null,
   async init(vk) {
-    schedule.scheduleJob('0 30 * * *', async () => {
+    schedule.scheduleJob('*/30 * * * *', async () => {
       const scheduleSubscribers = storeGetters.getScheduleSubscribers();
 
       if (scheduleSubscribers.length) {
-        const scheduleData = await this.getTodayRawSchedule();
+        const scheduleData = await this.getTodayRawSchedule(vk);
 
-        if (scheduleData) {
-          const { beginLesson } = scheduleData[0];
-          const todayDate = formatDate(new Date(), 'yyyy.MM.dd');
-          const todayFullDate = new Date(`${todayDate} ${beginLesson}`);
-          const notifyDate = removeFromDate(todayFullDate, { minutes: 10 });
+        if (scheduleData && scheduleData.length) {
+          const dateDiff = this.getDiffDateFromLesson(scheduleData[0].beginLesson);
 
-          await this.createSchedule(notifyDate, vk);
+          if (dateDiff >= 0 && dateDiff < 30)
+            await this.sendSchedule(vk);
         }
       }
     });
   },
-  async createSchedule(date, vk) {
+  getDiffDateFromLesson(beginLessonTime) {
+    const todayDate = this.getTodayDate();
+    const lessonDate = new Date(`${todayDate} ${beginLessonTime}`);
+
+    return getDateDiffInMinutes(lessonDate, new Date());
+  },
+  async sendSchedule(vk) {
     const scheduleSubscribers = storeGetters.getScheduleSubscribers();
     const scheduleData = await this.getTodaySchedule();
 
-    schedule.cancelJob(this.currentSchedule);
-
-    this.currentSchedule = schedule.scheduleJob(date, () => {
+    if (!scheduleData) {
+      await handleError('No schedule data', vk);
+    } else {
       each(scheduleSubscribers, (chatId) => {
         sendAsImage({ message: scheduleData, peerId: chatId, vk })
-          .catch((error) => handleError(error, vk));
+          .catch((err) => handleError(err, vk));
       });
-
-      schedule.cancelJob(this.currentSchedule);
-    });
+    }
   },
-  async getTodayRawSchedule() {
-    const today = formatDate(new Date(), serverDateFormat);
+  async getTodayRawSchedule(vk) {
+    const todayDate = this.getTodayDate();
 
-    return getRawSchedule(today, today);
+    return getRawSchedule(todayDate, todayDate)
+      .catch((err) => handleError(err, vk));
   },
   async getTodaySchedule() {
-    const today = formatDate(new Date(), serverDateFormat);
+    const todayDate = this.getTodayDate();
 
-    return getFormattedSchedule(['all'], { start: today, finish: today });
+    return getFormattedSchedule(['all'], { start: todayDate, finish: todayDate });
+  },
+  getTodayDate() {
+    return formatDate(new Date(), serverDateFormat);
   },
 };
