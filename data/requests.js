0b9a197ccccee45b75302e13dc9915330e06276d
Aladdin <g.tabroid@gmail.com>
2022-05-27 22:26:52 +0300

    v 1.29.0

--- data/requests.js
+++ data/requests.js
@@ -2,6 +2,5 @@ const { serverAddress } = require('../config');
 
 module.exports = {
   getUniversalUrl: (query) => `${serverAddress}/${query}`,
-  getActualityUrl: `${serverAddress}/getActuality`,
   getScheduleUrl : `${serverAddress}/getSchedule`,
 };
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,9 @@
-const getActuality = require('./getActuality');
 const getSchedule = require('./getSchedule');
 const getFormattedSchedule = require('./getFormattedSchedule');
 const createImage = require('./createImage');
 const sendAsImage = require('./sendAsImage');
 
 module.exports = {
-  getActuality,
   getSchedule,
   getFormattedSchedule,
   createImage,
