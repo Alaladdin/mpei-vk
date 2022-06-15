350648204fa4b46a34ab07c875e5a19761419278
Aladdin <g.tabroid@gmail.com>
2022-06-15 23:13:42 +0300

    v 1.30.1

--- cron/birthday/metadata.js
+++ cron/birthday/metadata.js
@@ -3,9 +3,16 @@ module.exports = {
     default: [
       'default_1.ogg',
       'default_2.mp3',
+      'default_3.ogg',
+    ],
+    161372337: [
+      'tabriz_1.ogg',
     ],
     425704393: [
       'drobot_1.ogg',
+    ],
+    160477317: [
+      'vika_1.ogg',
     ],
   },
 };
